afc134ed0a17dd09520c9949e4b71f7d8fadbb89
Neha Nishant Narula <nehanishant@Nehas-MacBook-Pro.local>
2026-06-24 13:31:40 -0400

    Commit of Spring AI project (Embeddings)

--- src/main/java/com/learn/AI/SpringAI/service/AIService.java
+++ src/main/java/com/learn/AI/SpringAI/service/AIService.java
@@ -5,8 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -14,8 +19,38 @@ import java.util.Map;
 public class AIService {
 
         private final ChatClient chatClient;
+        private final EmbeddingModel embeddingModel;
+        private final VectorStore vectorStore;
+
+    public float[] getEmbedding(String text){
+            return embeddingModel.embed(text);
+    }
+
+    public void ingestDataToVectorStore(){
+        /*Document document = new Document(text);
+        vectorStore.add(List.of(document));*/
+        List<Document> movies = List.of(
+                new Document("A wrongly convicted banker forms a lifelong bond with a fellow inmate while maintaining hope and planning a brilliant, decades-long escape.",
+                        Map.of("title","The Shawshank Redemption","genre","Drama","year",1994)),
+                new Document("The reluctant youngest son of a powerful New York mafia family takes control of the criminal empire after an assassination attempt on his father.",
+                        Map.of("title","The Godfather","genre","Crime,Drama","year",1972)),
+                new Document("TBatman must face psychological warfare and absolute chaos when a sadistic, unpredictable anarchist known as the Joker wreaks havoc on Gotham City.",
+                        Map.of("title","The Godfather","genre","Crime,Drama","year",1972))
+        );
+        vectorStore.add(movies);
+    }
+
+    public List<Document> similaritySearch(String text){
+        return vectorStore.similaritySearch(
+                    SearchRequest.builder()
+                            .query(text)
+                            .topK(2)
+                            .build()
+        );
+    }
 
         public String getJoke(String topic){
+
 
             String systemPrompt = """
                     You are a sarcastic joker, you make poetic jokes in 4 lines.
@@ -36,5 +71,7 @@ public class AIService {
 
             return response.text();
         }
+
+
 
 }
--- src/test/java/com/learn/AI/SpringAI/service/AIServiceTests.java
+++ src/test/java/com/learn/AI/SpringAI/service/AIServiceTests.java
@@ -15,4 +15,28 @@ public class AIServiceTests {
         var joke = aiService.getJoke("Cats");
         System.out.println(joke);
      }
+
+     @Test
+     public void testEmbeddedText(){
+         var embed = aiService.getEmbedding("This is about learning Spring AI");
+         System.out.println("Embeeded value is: "+embed);
+         System.out.println("Length of embeded test is: "+embed.length);
+         for(float e:embed){
+             System.out.println("Length of each embeded character is: "+e);
+         }
+     }
+
+     @Test
+    public void testStoreData(){
+         aiService.ingestDataToVectorStore();
+     }
+
+    @Test
+    public void testSimilaritySearch(){
+        var response = aiService.similaritySearch("crime movie");
+        for(var e:response){
+            System.out.println(e);
+        }
+
+    }
 }
